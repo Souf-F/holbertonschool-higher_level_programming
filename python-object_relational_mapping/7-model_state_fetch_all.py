e5a43481bec51f342933ad40d9d2230d7fa99d4e
Souf-F <sfecom.31000@gmail.com>
2026-06-08 22:15:56 +0200

    Refactor 7-model_state_fetch_all.py to streamline database connection and querying logic

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects from the database.
-"""
-
+"""Script that lists all State objects from the database."""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 
-
-def main():
-    """Main function."""
-    if len(sys.argv) != 4:
-        return
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine(
-        f'mysql+pymysql://{username}:{password}@localhost:3306/{database}',
-        pool_pre_ping=True
-    )
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    session.close()
-
-
 if __name__ == "__main__":
-    main()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    with Session(engine) as session:
+        for state in session.query(State).order_by(State.id).all():
+            print("{}: {}".format(state.id, state.name))
