490cea524c63428205dad51a97a69fc4eadf15df
Souf-F <sfecom.31000@gmail.com>
2026-06-29 11:10:13 +0200

    Add 8-square.js to print a square of 'X' characters based on user input

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (!Number.isInteger(size)) {
+  console.log('Missing size');
+} else if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
